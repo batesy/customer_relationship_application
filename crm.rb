26e127d59b786ab05e080bd91e3ee3c6bbeb6741
batesy <gbatesy@gmail.com>
2013-10-25 17:45:56 -0400

    Delete Contact feature added.

--- crm.rb
+++ crm.rb
@@ -9,7 +9,7 @@ class CRM
 
   def print_main_menu
     puts "[1] Add a new contact"
-    puts "[2] Modify an existing contact"
+    puts "[2] Edit an existing contact"
     puts "[3] Display a contact"
     puts "[4] Display all the contacts"
     puts "[5] Display an attribute"
@@ -19,9 +19,11 @@ class CRM
   end
 
   def main_menu
+    begin
     print_main_menu
     user_selected = gets.to_i
     call_option(user_selected)
+    end while user_selected != 7 
   end
 
   def call_option(user_selected)
@@ -29,7 +31,7 @@ class CRM
     when 1
       add_new_contact
     when 2
-      modify_existing_contact
+      edit_contact
     when 3
       display_contact
     when 4
@@ -57,9 +59,35 @@ class CRM
     main_menu
   end
 
+  def delete_a_contact
+    display_all_contacts
+    puts "Which contact do you want to delete? Enter ID: "
+    contact_to_delete = gets.to_i
+    Rolodex.delete_contact(contact_to_delete)
+  end
+
   def display_all_contacts
     Rolodex.display_all_contacts
+    puts
   end
+
+  def display_a_contact
+    puts "Enter an ID to display: "
+    contact_to_display = gets.to_i
+#    Rolodex.display_contact(contact_to_display)
+  end
+
+  def edit_contact
+    puts "Which contact do you want to edit?"
+    display_all_contacts
+    puts "Enter ID: "
+    contact_to_edit = gets.to_i
+    puts "You want to edit contact: #{contact_to_edit}?"
+#   Finish Later
+  end
+
+
+
 end
 
 
--- rolodex.rb
+++ rolodex.rb
@@ -8,12 +8,19 @@ class Rolodex
     @id += 1
   end
 
+  def self.delete_contact(contact_to_delete)
+    contacts.delete_if {|i| i.id == contact_to_delete }
+  end
+
   def self.contacts
     @contacts
   end
 
   def self.display_all_contacts
-    self.contacts.each {|i| puts "ID: #{i.id}  #{i.first_name} #{i.last_name}  Email: #{i.email}  Notes: #{i.note}" }
+    contacts.each {|i| puts "ID: #{i.id}  #{i.first_name} #{i.last_name}  Email: #{i.email}  Notes: #{i.note}" }
   end
 
+  def self.display_contact
+    contacts.each {}
+  end
 end
