5dca8dc5cce83f2ce4aa2f163d017358d93d6faf
batesy <gbatesy@gmail.com>
2013-10-25 12:20:28 -0400

    Initial Commit. Displays main menue.

---
+++ crm.rb
@@ -0,0 +1,11 @@
+def print_main_menu
+  puts "[1] Add Contact"
+  puts "[2] Modify a Contact"
+  puts "[3] Display All Contacts"
+  puts "[4] Display Contact"
+  puts "[5] Display Attribute"
+  puts "[6] Delete"
+  puts "[7] Exit"
+end
+
+print_main_menu
